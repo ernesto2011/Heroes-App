1b371630ef89812c5d8cc130ca09c37a00c3fed9
ernesto2011 <skillmx107@gmail.com>
2024-12-17 14:27:08 -0600

    hamburguer menu

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -6,7 +6,7 @@ import { useMemo } from 'react'
 export const HeroList = ({publisher}) => {
     const heroes = useMemo(()=> getHeroesPublisher(publisher),[publisher]) 
   return (
-    <div className='row rows-cols-1 row-cols-md-3 g-3'>
+    <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
         {
             heroes.map(heroe => (
                 <HeroCard key={heroe.id} {...heroe} />
--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -4,17 +4,18 @@ import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../Auth';
 
 export const NavBar = () => {
-const {user, logOut} = useContext(AuthContext)
-const navigate = useNavigate();
-const onLogOut = () => {
-    logOut()
-    navigate('/login',{
-        replace: true
-    });
-}
+    const { user, logOut } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onLogOut = () => {
+        logOut();
+        navigate('/login', {
+            replace: true
+        });
+    };
+
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-            
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-2">
             <Link 
                 className="navbar-brand" 
                 to="/"
@@ -22,43 +23,57 @@ const onLogOut = () => {
                 Asociaciones
             </Link>
 
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
+            <button 
+                className="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#navbarNav" 
+                aria-controls="navbarNav" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="navbar-nav">
                     <NavLink 
-                        className={({isActive})=>`nav-item nav-link ${isActive ? 'active':''}` }
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink 
-                        className={({isActive})=>`nav-item nav-link ${isActive ? 'active':''}` } 
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} 
                         to="/dc"
                     >
                         DC
                     </NavLink>
+
                     <NavLink 
-                        className={({isActive})=>`nav-item nav-link ${isActive ? 'active':''}` } 
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} 
                         to="/search"
                     >
                         Search
                     </NavLink>
                 </div>
-            </div>
-
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+                <div className="collapse navbar-collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <span className='nav-item nav-link text-primary'>
+                    <span className="nav-item nav-link text-primary">
                         {user?.name}
                     </span>
                     <button
-                    onClick={onLogOut}
-                    className='nav-item nav-link btn'>
+                        onClick={onLogOut}
+                        className="nav-item nav-link btn"
+                    >
                         LogOut
                     </button>
                 </ul>
             </div>
+            </div>
+
+           
         </nav>
-    )
+    );
 }
